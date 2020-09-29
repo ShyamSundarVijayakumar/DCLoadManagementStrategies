f20c4119bf7bb80db8f147dd9588e720e9add11a
ShyamSundarVijayakumar <shyamsundar1791993@gmail.com>
2020-09-30 00:33:10 +0200

    Removed unnecessary code

--- Desktop/Cloudsim/cloudsim-plus/src/main/java/hierarchicalarchitecture/localcontrollerdaas/DynamicPopulationDAAS.java
+++ Desktop/Cloudsim/cloudsim-plus/src/main/java/hierarchicalarchitecture/localcontrollerdaas/DynamicPopulationDAAS.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package hierarchicalarchitecture.localcontrollerdaas;
 
 import java.util.ArrayList;
@@ -12,30 +9,21 @@ import java.util.stream.IntStream;
 
 
 /**
+ * This class is dedicated for creating the population for genetic algorithm in DaaS application.
+ * 
  * @author Shyam Sundar V
- *
  */
 public class DynamicPopulationDAAS {
 
-	public ArrayList<DynamicChromosomeDAAS> chromosomes;
-	
+	public ArrayList<DynamicChromosomeDAAS> chromosomes;	
 	Map<DynamicChromosomeDAAS, Double> chromosomewithFitness = new HashMap<DynamicChromosomeDAAS,Double>();
 	Map<DynamicChromosomeDAAS, Double> sortedPopulation;
-//	List<Host> HostList;
-//	List<Vm> VmList;
-	
-/*	public DynamicPopulationDAAS(int populationSize, List<Host> hostList, List<Vm> vmList) {//Constructor added------------------------------------->
-		chromosomes = new ArrayList<DynamicChromosomeDAAS>(populationSize);
-		this.HostList = hostList;
-		this.VmList = vmList;
-	}*/
 	
 	public DynamicPopulationDAAS(int populationSize) {
 		chromosomes = new ArrayList<DynamicChromosomeDAAS>(populationSize);
 	}
 	
 	public DynamicPopulationDAAS intialize(int populationSize) {
-//		System.out.println("POPULATION INITIALIZATION");
 		IntStream.range(0, populationSize).forEach(i ->	chromosomes.add(i, new DynamicChromosomeDAAS().initialize(i)));		
 		return this;
 	}
@@ -44,12 +32,6 @@ public class DynamicPopulationDAAS {
 		return chromosomes;
 	}
 	
-	/**
-	 * make a map for chromosomeDAAS and their fitness values;
-	 *  sort them in ascending order of their fitness values
-	 * @param chromosomeDAAS
-	 * @return 
-	 */
 	public DynamicPopulationDAAS sortChromosomesByFitness() {
 		chromosomes.sort((chromosome1, chromosome2) -> {
 			int flag = 0;
@@ -57,10 +39,8 @@ public class DynamicPopulationDAAS {
 			if(chromosome1.getFitness() < chromosome2.getFitness()) flag = -1;
 			return flag;
 		});
-		
-//		 sortChromosomeByFitness();
-		return this;	
-		
+	
+		return this;
 	}
 	
 	/**
@@ -71,14 +51,13 @@ public class DynamicPopulationDAAS {
 		for(int i=0; i< chromosomes.size(); i++) {
 			chromosomewithFitness.put(getChromosomes().get(i), getChromosomes().get(i).getFitness());
 		}
-			 sortedPopulation = chromosomewithFitness.entrySet().stream().sorted(Map.Entry.<DynamicChromosomeDAAS, Double>comparingByValue().reversed())
+		
+		sortedPopulation = chromosomewithFitness.entrySet().stream().sorted(Map.Entry.<DynamicChromosomeDAAS, Double>comparingByValue().reversed())
 		.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1,e2) -> e2,LinkedHashMap::new));
-//		 System.out.println("chromosome+fitness...."+sortedPopulation);
-	 return sortedPopulation;
+	 
+		return sortedPopulation;
 	}
 
-	
-	
 	public Map<DynamicChromosomeDAAS, Double> getSortedPopulation() {
 		return sortedPopulation;
 	}
