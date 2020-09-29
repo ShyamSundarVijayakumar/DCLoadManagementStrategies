391f23efccf1ba70e4b11a084352d55f72adc8fe
ShyamSundarVijayakumar <shyamsundar1791993@gmail.com>
2020-09-30 00:46:34 +0200

    Removed unused code and updated code documentation.

--- Desktop/Cloudsim/cloudsim-plus/src/main/java/hierarchicalarchitecture/localcontrollerdaas/EvolutionDAAS.java
+++ Desktop/Cloudsim/cloudsim-plus/src/main/java/hierarchicalarchitecture/localcontrollerdaas/EvolutionDAAS.java
@@ -1,8 +1,4 @@
-/**
- * 
- */
 package hierarchicalarchitecture.localcontrollerdaas;
-
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,39 +12,37 @@ import org.cloudbus.cloudsim.vms.Vm;
 
 
 /**
+ * This class is dedicated for depicting the evolution process of genetic algorithm for the initial vm placement in DaaS application.
+ *  
  * @author Shyam Sundar V
- *
  */
 public class EvolutionDAAS {
-
-	
 	public int generationsEvolved;
 	private static RandomGenerator randomGenerator = new JDKRandomGenerator();
-	    /** the rate of crossover for the algorithm. */
-	    private final double crossoverRate = 0.5;
-	    /** the rate of mutation for the algorithm. */
-	    private final double mutationRate = 0.5;
-	    /** Inital placement elitism. This way we keep the fittest chromosome from one generation to the other 
-	     * generation unchanged*/
-	    private final int ELITE_CHROMOSOMES = 1;
-	    
-	    private final int TOURNAMENT_SELECTION_SIZE = 4; 
-	    
-	    private final int CHROMOSOME_LENGTH;
-
-	 public EvolutionDAAS(int INITIAL_VMS) {
-		 this.CHROMOSOME_LENGTH = INITIAL_VMS;
-		 
+	private final double crossoverRate = 0.5;
+	private final double mutationRate = 0.5;
+	/** Inital placement elitism. This way we keep the fittest chromosome from one generation to the other generation unchanged*/
+	private final int ELITE_CHROMOSOMES = 1;
+	private final int TOURNAMENT_SELECTION_SIZE = 4;     
+	private final int CHROMOSOME_LENGTH;
+	private List<Host> Host_List = new ArrayList<>();
+	private List<Vm> Vm_List = new ArrayList<>();
+ 
+	public EvolutionDAAS(int INITIAL_VMS) {
+		 this.CHROMOSOME_LENGTH = INITIAL_VMS;	 
 	 }
-	 private List<Host> Host_List = new ArrayList<>();
-	 private List<Vm> Vm_List = new ArrayList<>();
+	 
 	public PopulationDAAS evolve(PopulationDAAS populationDAAS, List<Host> hostlist,List<Vm> vmlist) {
-
-		this.Host_List=hostlist;
-		this.Vm_List=vmlist;      
+		this.Host_List = hostlist;
+		this.Vm_List = vmlist;      
 		return mutatePopulation(crossoverPopulation(populationDAAS));
-			}
+	}
 	
+	/**
+	 * This method returns the population of chromosomes that consists of chromosomes after crossover.
+	 * @param populationDAAS
+	 * @return
+	 */
 	public PopulationDAAS crossoverPopulation(PopulationDAAS populationDAAS){
 		PopulationDAAS crossoverpopulation = new PopulationDAAS(populationDAAS.getChromosomes().length, CHROMOSOME_LENGTH);
 		/*
@@ -66,6 +60,11 @@ public class EvolutionDAAS {
 	}
 	
 	
+	/**
+	 * This method returns the population of chromosomes after their mutation process.
+	 * @param populationDAAS
+	 * @return
+	 */
 	public PopulationDAAS mutatePopulation(PopulationDAAS populationDAAS) {
 		PopulationDAAS mutatepopulation = new PopulationDAAS(populationDAAS.getChromosomes().length, CHROMOSOME_LENGTH);
 		for(int i=0; i < ELITE_CHROMOSOMES; i++) {
@@ -85,7 +84,7 @@ public class EvolutionDAAS {
 	}
 	
 	/**
-	 * For initial placement, random gene selection from each parent chromosome
+	 * Uniform crossover strategy is followed here.
 	 * @param chromosome1
 	 * @param chromosome2
 	 * @return
@@ -95,7 +94,6 @@ public class EvolutionDAAS {
 		for(int i=0; i< chromosome1.getGenes().length; i++) {
 			if(Math.random() < crossoverRate) {
 			 crossoverChromosome.getGenes()[i] = chromosome1.getGenes()[i];
-			 
 			 }
 		else {
 				 crossoverChromosome.getGenes()[i] = chromosome2.getGenes()[i];
@@ -106,6 +104,11 @@ public class EvolutionDAAS {
 		return crossoverChromosome;
 	}
 	
+	/**
+	 * Mutation is performed based on the mutation rate. For all the availability violated setvers a vm will be selected and mapped to another random host.
+	 * @param Chromosome
+	 * @return
+	 */
 	public ChromosomeDAAS mutateChromosome(ChromosomeDAAS Chromosome) {	
 		ChromosomeDAAS mutateChromosome = new ChromosomeDAAS(CHROMOSOME_LENGTH);
 		int index;
@@ -134,31 +137,14 @@ public class EvolutionDAAS {
 						  mutateChromosome.genes[VMs] = Host;
 					  }
 				  }
-				 
 			}
 		}
 		mutateChromosome.setHostlistAndVmlist(Host_List, Vm_List);
 		mutateChromosome.makeVMServerMapDuringCrossoverAndMutation(mutateChromosome.getGenes());
-		
-		/*
-		 * for(int vs : mutateChromosome.availabitlityViolatedServers) { List<Integer>
-		 * serverVms = mutateChromosome.serverwithVMList.get(vs); for(Integer VMs :
-		 * serverVms) { if(!Vm_List.get(VMs).isCreated()) {
-		 * System.out.println("Reached"); } } }
-		 */
+	
 		return mutateChromosome;
 	}
 
-	/*
-	 * private int generateAnotherRandomHost(ChromosomeDAAS Chromosome, int
-	 * IgnoreThisHost) { int RandomHostForGene =
-	 * ThreadLocalRandom.current().nextInt(0,Host_List.size() );
-	 * 
-	 * while((IgnoreThisHost == RandomHostForGene)) { RandomHostForGene =
-	 * ThreadLocalRandom.current().nextInt(0, Host_List.size()); }
-	 * 
-	 * return RandomHostForGene; }
-	 */
 	/**
 	 * @param chromosomeDAAS
 	 * @param mutateChromosome
@@ -166,15 +152,9 @@ public class EvolutionDAAS {
 	 * @param index
 	 */
 	private void swapingGenesforVMsnotCreated(ChromosomeDAAS chromosomeDAAS, ChromosomeDAAS mutateChromosome, int i,int index) {
-		if(Vm_List.get(i).isCreated()) {//mutateChromosome.getGenes()[i])
+		if(Vm_List.get(i).isCreated()) {
 			 mutateChromosome.getGenes()[i] = chromosomeDAAS.getGenes()[i];
 		}else {
-	//		if(Math.random() < 0.5) {
-		//		mutateChromosome.getGenes()[i] = mutateChromosome.getGenes()[index];
-				//	Array.set(mutateChromosome.getGenes(), mutateChromosome.getGenes()[i], mutateChromosome.getGenes()[index]);
-	//		}else {
-		//		mutateChromosome.getGenes()[i] = chromosomeDAAS.getGenes()[i];	
-		//	}
 			if(Math.random() < 0.5) {
 				int RandomHostForGene = ThreadLocalRandom.current().nextInt(0, chromosomeDAAS.SERVERS);	
 				mutateChromosome.getGenes()[i] = RandomHostForGene;
@@ -187,8 +167,8 @@ public class EvolutionDAAS {
 	private int generateRandomHostIgnoringSelectedHost(ChromosomeDAAS chromosomeDAAS, int IgnoreThisHost) {
 		int RandomHostForGene = ThreadLocalRandom.current().nextInt(0, chromosomeDAAS.SERVERS);				
 
-		if(! (chromosomeDAAS.vmToServerMap.keySet().size() == 1)) {
-			while((IgnoreThisHost == RandomHostForGene) && chromosomeDAAS.availabitlityViolatedServers.contains(RandomHostForGene)) {//|| (!chromosomeDAAS.vmToServerMap.containsKey(RandomHostForGene)
+		if(!(chromosomeDAAS.vmToServerMap.keySet().size() == 1)) {
+			while((IgnoreThisHost == RandomHostForGene) && chromosomeDAAS.availabitlityViolatedServers.contains(RandomHostForGene)) {
 				RandomHostForGene = ThreadLocalRandom.current().nextInt(0, chromosomeDAAS.SERVERS);
 			}
 		}else {
@@ -196,18 +176,9 @@ public class EvolutionDAAS {
 		}
 		return RandomHostForGene;
 	}
-//	
-//	private int GenerateRandomHostIgnoringInactive(ChromosomeDAAS chromosomeDAAS) {
-//		int RandomHost = ThreadLocalRandom.current().nextInt(0, chromosomeDAAS.SERVERS);				
-//
-//		while((!chromosomeDAAS.getServersWithVmList().containsKey(RandomHost)) && (Host_List.get(RandomHost).getVmList().isEmpty())) {
-//			
-//			RandomHost = ThreadLocalRandom.current().nextInt(0, chromosomeDAAS.SERVERS);	
-//		}
-//		return RandomHost;
-//	}
 	
 	/** 
+	 * Tournament selection strategy is followed here.
 	 * @param populationDAAS
 	 * @return
 	 */
@@ -220,43 +191,45 @@ public class EvolutionDAAS {
 		tournamentPopulation.sortChromosomesByFitness();
 		return tournamentPopulation;
 	}
-	 /**
+	 
+	/**
      * Set the (static) random generator.
-     *
      * @param random random generator
      */
     public static synchronized void setRandomGenerator(final RandomGenerator random) {
         randomGenerator = random;
     }
+    
     /**
      * Returns the (static) random generator.
-     *
      * @return the static random generator shared by GA implementation classes
      */
     public static synchronized RandomGenerator getRandomGenerator() {
         return randomGenerator;
     }
-/**
- * Returns the crossover rate.
- * @return crossover rate
- */
-public double getCrossoverRate() {
-    return crossoverRate;
-}
-/**
- * Returns the mutation rate.
- * @return mutation rate
- */
-public double getMutationRate() {
-    return mutationRate;
-}
-/**
- * Returns the number of generations evolved to reach {@link StoppingCondition} in the last run.
- *
- * @return number of generations evolved
- */
-public int getGenerationsEvolved() {
-    return generationsEvolved;
-}	
+
+    /**
+	 * Returns the crossover rate.
+	 * @return crossover rate
+	 */
+	public double getCrossoverRate() {
+	    return crossoverRate;
+	}
+	
+	/**
+	 * Returns the mutation rate.
+	 * @return mutation rate
+	 */
+	public double getMutationRate() {
+	    return mutationRate;
+	}
+	
+	/**
+	 * Returns the number of generations evolved to reach {@link StoppingCondition} in the last run.
+	 * @return number of generations evolved
+	 */
+	public int getGenerationsEvolved() {
+	    return generationsEvolved;
+	}	
 	
 }
