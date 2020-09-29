a8993dee9c92468b99dc960b073f182714689d40
ShyamSundarVijayakumar <shyamsundar1791993@gmail.com>
2020-09-30 00:26:26 +0200

    Updated code documentation

--- Desktop/Cloudsim/cloudsim-plus/src/main/java/hierarchicalarchitecture/localcontrollerdaas/DynamicEvolutionDAAS.java
+++ Desktop/Cloudsim/cloudsim-plus/src/main/java/hierarchicalarchitecture/localcontrollerdaas/DynamicEvolutionDAAS.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package hierarchicalarchitecture.localcontrollerdaas;
 
 import java.util.ArrayList;
@@ -13,14 +10,11 @@ import org.apache.commons.math3.random.JDKRandomGenerator;
 import org.apache.commons.math3.random.RandomGenerator;
 
 /**
+ * This class is dedicated to depict the evolution process of genetic algorithm for dynamic VM placement of DaaS application.
+ *  
  * @author Shyam Sundar V
- *
  */
 public class DynamicEvolutionDAAS {
-	/**
-	 * This way we keep the fittest chromosome from one generation to the other
-	 * generation unchanged
-	 */
 	private final int ELITE_CHROMOSOMES = 1;
 	private final int TOURNAMENT_SELECTION_SIZE = 3;
 	/** the rate of crossover for the algorithm. */
@@ -28,34 +22,36 @@ public class DynamicEvolutionDAAS {
 	/** the rate of mutation for the algorithm. */
 	private final double mutationRate = 0.7;
 	private static RandomGenerator randomGenerator = new JDKRandomGenerator();
-
 	private Random rand = new Random();
 	long crossPoint1;
 	long crossPoint2;
+	DynamicChromosomeDAAS mutateChromosome;
+	List<Long> VMID = new ArrayList<>();
 
 	public DynamicPopulationDAAS evolve(DynamicPopulationDAAS population) {
-
 		return mutatePopulation(crossoverPopulation(population));
 	}
 
+	/**
+	 * This method returns the population of chromosomes that consists of chromosomes after crossover. 
+	 * 
+	 * @param population
+	 * @return
+	 */
 	private DynamicPopulationDAAS crossoverPopulation(DynamicPopulationDAAS population) {
 		DynamicPopulationDAAS crossoverpopulation = new DynamicPopulationDAAS(population.getChromosomes().size());
 		/*
 		 * Exclude elite chromosomeDAAS
 		 */
-		IntStream.range(0, ELITE_CHROMOSOMES)
-				.forEach(i -> crossoverpopulation.getChromosomes().add(population.getChromosomes().get(i)));
+		IntStream.range(0, ELITE_CHROMOSOMES).forEach(i -> crossoverpopulation.getChromosomes().add(population.getChromosomes().get(i)));
 
 		for (int i = ELITE_CHROMOSOMES; i < population.getChromosomes().size(); i++) {
 			/*
 			 * Selecting Parent chromosomeDAAS to perform crossover
 			 */
 			if (crossoverRate > Math.random()) {
-				DynamicChromosomeDAAS chromosome1 = selectPopulation(population).sortChromosomesByFitness()
-						.getChromosomes().get(0);
-				DynamicChromosomeDAAS chromosome2 = selectPopulation(population).sortChromosomesByFitness()
-						.getChromosomes().get(0);
-
+				DynamicChromosomeDAAS chromosome1 = selectPopulation(population).sortChromosomesByFitness().getChromosomes().get(0);
+				DynamicChromosomeDAAS chromosome2 = selectPopulation(population).sortChromosomesByFitness().getChromosomes().get(0);
 				crossoverpopulation.getChromosomes().add(crossoverChromosome(chromosome1, chromosome2));
 			} else {
 				crossoverpopulation.getChromosomes().add(population.getChromosomes().get(i));
@@ -66,6 +62,7 @@ public class DynamicEvolutionDAAS {
 	}
 
 	/**
+	 * This method returns the population of chromosomes after their mutation process.
 	 * 
 	 * @param population
 	 * @return
@@ -75,8 +72,7 @@ public class DynamicEvolutionDAAS {
 		/*
 		 * Exclude elite chromosomeDAAS
 		 */
-		IntStream.range(0, ELITE_CHROMOSOMES)
-				.forEach(i -> mutatePopulation.getChromosomes().add(population.getChromosomes().get(i)));
+		IntStream.range(0, ELITE_CHROMOSOMES).forEach(i -> mutatePopulation.getChromosomes().add(population.getChromosomes().get(i)));
 		for (int i = ELITE_CHROMOSOMES; i < population.getChromosomes().size(); i++) {
 			mutatePopulation.getChromosomes().add(mutateChromosome(population.getChromosomes().get(i)));
 		}
@@ -91,9 +87,7 @@ public class DynamicEvolutionDAAS {
 	 * @param chromosome2
 	 * @return
 	 */
-	private DynamicChromosomeDAAS crossoverChromosome(DynamicChromosomeDAAS chromosome1,
-			DynamicChromosomeDAAS chromosome2) {
-
+	private DynamicChromosomeDAAS crossoverChromosome(DynamicChromosomeDAAS chromosome1, DynamicChromosomeDAAS chromosome2) {
 		DynamicChromosomeDAAS crossoverChromosome = new DynamicChromosomeDAAS();
 		int RandomVm = randomGenerator.nextInt(GADriverDaas.sourcevmList.size());
 		crossPoint1 = GADriverDaas.sourcevmList.get(RandomVm);
@@ -109,12 +103,13 @@ public class DynamicEvolutionDAAS {
 			}
 		}
 		// .. and crosspoint1 is lower than crosspoint2
-		// interchanging values
+
 		if (crossPoint2 < crossPoint1) {
 			Long temp = crossPoint1;
 			crossPoint1 = crossPoint2;
 			crossPoint2 = temp;
 		}
+		
 		GADriverDaas.dynamicVmHostMap.forEach((vm, server) -> {
 			if (vm < crossPoint1 || vm > crossPoint2) {
 				crossoverChromosome.getGenes().put(vm, chromosome1.getGenes().get(vm));
@@ -127,15 +122,11 @@ public class DynamicEvolutionDAAS {
 	}
 
 	/**
-	 * Method to apply mutation. We apply mutation based on the mutation rate and we
-	 * mutate vms on the current active hosts
+	 * Mutation is performed based on the mutation rate. mutate vms that are not yet created on the current active hosts.
 	 * 
 	 * @param chromosome
 	 * @return
 	 */
-	DynamicChromosomeDAAS mutateChromosome;
-	List<Long> VMID = new ArrayList<>();
-
 	private DynamicChromosomeDAAS mutateChromosome(DynamicChromosomeDAAS chromosome) {
 		mutateChromosome = new DynamicChromosomeDAAS();
 		LocalControllerDaas.VmstoMigrateFromOverloadedUnderloadedHosts.forEach(vm -> {
@@ -149,66 +140,11 @@ public class DynamicEvolutionDAAS {
 			} else {
 				mutateChromosome.getGenes().put(vm, chromosome.getGenes().get(vm));
 			}
-			/*
-			 * if(VMID.isEmpty()) { mutateChromosome.getGenes().put(vm,
-			 * chromosome.getGenes().get(vm)); }
-			 */
-
-			/*
-			 * if(VMID != null) { if(VMID.contains(vm)){ if(Math.random() < mutationRate) {
-			 * mutateChromosome.getGenes().put(vm,
-			 * GeneticAlgorithmDriverDaas.targetHostList.get(rand.nextInt(
-			 * GeneticAlgorithmDriverDaas.targetHostList.size()))); VMID.remove(vm); }else {
-			 * mutateChromosome.getGenes().put(vm, chromosome.getGenes().get(vm)); } }else {
-			 * mutateChromosome.getGenes().put(vm, chromosome.getGenes().get(vm)); } }
-			 */
-			/*
-			 * for(Long number : VMID) { if (number == vm) { if(Math.random() <
-			 * mutationRate) { mutateChromosome.getGenes().put(vm,
-			 * GeneticAlgorithmDriverDaas.targetHostList.get(rand.nextInt(
-			 * GeneticAlgorithmDriverDaas.targetHostList.size()))); VMID.remove(number);
-			 * break; }else { VMID.remove(number); mutateChromosome.getGenes().put(vm,
-			 * chromosome.getGenes().get(vm)); break; }
-			 * 
-			 * } else if (number != vm) { mutateChromosome.getGenes().put(vm,
-			 * chromosome.getGenes().get(vm)); break; } }
-			 */
 
 		});
 
 		mutateChromosome.serverVMMapSource(mutateChromosome.getGenes());
-
-		/*
-		 * if(mutateChromosome.slaViolations() != 0){
-		 * while(mutateChromosome.slaViolations() != 0) { mutateChromosome = new
-		 * DynamicChromosomeDAAS();
-		 * LocalControllerDaas.VmstoMigrateFromOverloadedUnderloadedHosts.forEach(vm ->
-		 * { VMID.add(vm.getId()); }); //
-		 * System.out.println(TestDriver.sourcehostList.get(randomGenerator.nextInt(
-		 * TestDriver.sourcehostList.size())));
-		 * GADriverDaas.dynamicVmHostMap.forEach((vm,server) -> {
-		 * Collections.sort(VMID); if(VMID.isEmpty()) {
-		 * mutateChromosome.getGenes().put(vm, chromosome.getGenes().get(vm)); }
-		 * 
-		 * if(VMID != null) { fillGenes(chromosome, vm); } else {
-		 * mutateChromosome.getGenes().put(vm, chromosome.getGenes().get(vm)); }
-		 * for(Long number : VMID) { if (number == vm) { if(Math.random() <
-		 * mutationRate) { mutateChromosome.getGenes().put(vm,
-		 * GeneticAlgorithmDriverDaas.targetHostList.get(rand.nextInt(
-		 * GeneticAlgorithmDriverDaas.targetHostList.size()))); VMID.remove(number);
-		 * break; }else { VMID.remove(number); mutateChromosome.getGenes().put(vm,
-		 * chromosome.getGenes().get(vm)); break; }
-		 * 
-		 * } else if (number != vm) { mutateChromosome.getGenes().put(vm,
-		 * chromosome.getGenes().get(vm)); break; } }
-		 * 
-		 * }); mutateChromosome.serverVMMapSource(mutateChromosome.getGenes()); }
-		 
-	}*/
-//		System.out.println("From mutate chromosome exit*****************************************************************");
-
-	return mutateChromosome;
-
+		return mutateChromosome;
 	}
 
 	/**
@@ -238,21 +174,18 @@ public class DynamicEvolutionDAAS {
 	}
 
 	/**
+	 * population seleection follows a tournament selection strategy.
+	 * 
 	 * @param population
 	 * @return
 	 */
 	public DynamicPopulationDAAS selectPopulation(DynamicPopulationDAAS population) {
-
 		DynamicPopulationDAAS tournamentPopulation = new DynamicPopulationDAAS(TOURNAMENT_SELECTION_SIZE);
-		// .intialize(TOURNAMENT_SELECTION_SIZE);
 		for (int i = 0; i < TOURNAMENT_SELECTION_SIZE; i++) {
-			tournamentPopulation.getChromosomes()
-					.add(population.getChromosomes().get((int) (Math.random() * population.getChromosomes().size())));
-//			set(i, population.getChromosomes().get((int)(Math.random()*population.getChromosomes().size())));
+			tournamentPopulation.getChromosomes().add(population.getChromosomes().get((int) (Math.random() * population.getChromosomes().size())));
 		}
 
 		tournamentPopulation.sortChromosomesByFitness();
 		return tournamentPopulation;
 	}
-
 }
